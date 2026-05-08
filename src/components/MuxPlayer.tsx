ff3be77ed5f14f1a0371354f686a4c83a71eb093
Verbum <46956900+georgy4932@users.noreply.github.com>
2026-05-09 00:28:44 +0100

    Update MuxPlayer.tsx

--- src/components/MuxPlayer.tsx
+++ src/components/MuxPlayer.tsx
@@ -6,32 +6,26 @@ type Props = {
 };
 
 export default function MuxPlayer({ playbackId, title }: Props) {
-  const src = `https://stream.mux.com/${playbackId}.m3u8`;
-
   return (
     <div
       style={{
         width: "100%",
         aspectRatio: "16/9",
         background: "#000",
-        position: "relative",
       }}
     >
-      <video
-        controls
-        autoPlay={false}
-        playsInline
+      <iframe
+        src={`https://player.mux.com/${playbackId}`}
         title={title}
+        allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture"
+        allowFullScreen
         style={{
           width: "100%",
           height: "100%",
+          border: "none",
           display: "block",
-          accentColor: "#C9A84C",
         }}
-      >
-        <source src={src} type="application/x-mpegURL" />
-        Your browser does not support video playback.
-      </video>
+      />
     </div>
   );
 }
