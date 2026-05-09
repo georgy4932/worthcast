bbaddaa7f39bdf0bc546a0d364768584a0e5e939
Verbum <46956900+georgy4932@users.noreply.github.com>
2026-05-09 22:59:00 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main>
+      <main className="home-page">
         <Hero />
         <Ticker />
         <VideoGrid />
